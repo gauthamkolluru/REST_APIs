2c75bdefab4b83e738f57647e0f70a124d0e9fce
Sai Gautham Kolluru <skolluru@india.avalon.net>
2018-09-04 18:41:13 +0530

    added node_modules to gitignore & removed unnecessary statements from app.js

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ routes(app);
 
 var server = app.listen(3000,function(){
     console.log('app running on port', server.address().port);
-    console.log('Gautham')
+    // console.log('Gautham');
 });
